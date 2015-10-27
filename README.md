1f732605022a02f4f142dd68640cc8bf1f7028de
Henrik Sjööh <henriiik@gmail.com>
2015-10-27 19:57:04 +0100

    rename to docker-linter

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Windows
+# Docker Linter
+lint your shit in docker
+
+## OSX
+```
+code --extensionDevelopmentPath=/Users/henrik/Development/docker-linter --debugLanguageWorker=henriiik.docker-linter
+docker run --rm -it --name=perl --volume=/Users/henrik/Development/playground:/root/hello perl bash
+```
+## Windows
 ```
 code --extensionDevelopmentPath=C:\Users\henro\Documents\GitHub\vscode-docker-perl --debugLanguageWorker=henriiik.vscode-docker-perl
 docker run -it --rm --name perl --volume=/c/Users/playground:/root/hello perl bash
--- extension.ts
+++ extension.ts
@@ -3,59 +3,54 @@
 import { runSingleFileValidator, SingleFileValidator, InitializeResponse, IValidationRequestor, IDocument, Diagnostic, Severity, Position, Files } from 'vscode-languageworker';
 import { exec } from 'child_process';
 
-let envString;
-let envObj = {};
+let env = {};
 let envRegex = /export (.+)="(.+)"\n/g;
-let root;
+let root: string;
+let machine: string;
 
 let validator: SingleFileValidator = {
 	initialize: (rootFolder: string): Thenable<InitializeResponse> => {
 		root = rootFolder;
-		return new Promise<InitializeResponse>((resolve, reject) => {
-			let child = exec('docker-machine env default --shell bash', function(error, stdout, stderr) {
-				envString = stdout.toString();
-				resolve(null);
-			})
-		});
+		return Promise.resolve(null);
 	},
 	onConfigurationChange(settings: any, requestor: IValidationRequestor): void {
 		// VSCode settings have changed and the requested settings changes
 		// have been synced over to the language worker
+		
+		machine = settings["docker-linter"]["perl"]["machine"];
+		let child = exec(`docker-machine env ${machine} --shell bash`, function(error, stdout, stderr) {
+			let envString = stdout.toString();
+			let match;
+			while (match = envRegex.exec(envString)) {
+				env[match[1]] = match[2];
+			}
+		})
 
 		// Request re-validation of all open documents
 		requestor.all();
 	},
 	validate: (document: IDocument): Promise<Diagnostic[]> => {
-		// Validate a single document for diagnostic messages
-		// let match = null;
-		// while (match = envRegex.exec(envString)) {
-		// 	envObj[match[0]] = match[1];
-		// }
-		let match;
-		while (match = envRegex.exec(envString)) {
-			envObj[match[1]] = match[2];
-		}
 		return new Promise<Diagnostic[]>((resolve, reject) => {
-			let child = exec('docker exec perl perl -c -W /root/hello/test.pl', {
-				env: envObj
-			}, (error, stdout, stderr) => {
-				let result: Diagnostic[] = []
-				let message: string;
-				if (error) {
-					error.message.toString().split('\n').forEach(e => {
-						let hello = e.split(' at /root/hello/test.pl line ');
-						if (hello.length > 1) {
-							result.push({
-								start: { line: parseInt(hello[1]), character: 0 },
-								end: { line: parseInt(hello[1]), character: Number.MAX_VALUE },
-								severity: Severity.Error,
-								message: hello[0]
-							});
-						}
-					});
-				}
-				resolve(result)
-			})
+			let child = exec('docker exec perl perl -c -W /root/hello/test.pl',
+				{ env },
+				(error, stdout, stderr) => {
+					let result: Diagnostic[] = []
+					let message: string;
+					if (error) {
+						error.message.toString().split('\n').forEach(e => {
+							let hello = e.split(' at /root/hello/test.pl line ');
+							if (hello.length > 1) {
+								result.push({
+									start: { line: parseInt(hello[1]), character: 0 },
+									end: { line: parseInt(hello[1]), character: Number.MAX_VALUE },
+									severity: Severity.Error,
+									message: hello[0]
+								});
+							}
+						});
+					}
+					resolve(result)
+				})
 		});
 	}
 };
