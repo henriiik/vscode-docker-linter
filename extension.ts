b7518afc5d7b926ed66433598055d220f8b83afc
Henrik Sjööh <henriiik@gmail.com>
2015-10-28 07:32:46 +0100

    use spawn instead of exec

--- extension.ts
+++ extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { runSingleFileValidator, SingleFileValidator, InitializeResponse, IValidationRequestor, IDocument, Diagnostic, Severity, Position, Files } from 'vscode-languageworker';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 let env = {};
 let envRegex = /export (.+)="(.+)"\n/g;
@@ -11,14 +11,24 @@ let machine: string;
 let validator: SingleFileValidator = {
 	initialize: (rootFolder: string): Thenable<InitializeResponse> => {
 		root = rootFolder;
-		return Promise.resolve(null);
+		machine = "default";
+		return new Promise((resolve, reject) => {
+			exec(`docker-machine env ${machine} --shell bash`, function(error, stdout, stderr) {
+				let envString = stdout.toString();
+				let match;
+				while (match = envRegex.exec(envString)) {
+					env[match[1]] = match[2];
+				}
+			})
+			resolve(null)
+		})
 	},
 	onConfigurationChange(settings: any, requestor: IValidationRequestor): void {
 		// VSCode settings have changed and the requested settings changes
 		// have been synced over to the language worker
 		
 		machine = settings["docker-linter"]["perl"]["machine"];
-		let child = exec(`docker-machine env ${machine} --shell bash`, function(error, stdout, stderr) {
+		exec(`docker-machine env ${machine} --shell bash`, function(error, stdout, stderr) {
 			let envString = stdout.toString();
 			let match;
 			while (match = envRegex.exec(envString)) {
@@ -30,27 +40,26 @@ let validator: SingleFileValidator = {
 		requestor.all();
 	},
 	validate: (document: IDocument): Promise<Diagnostic[]> => {
+		let child = spawn('docker', 'exec perl perl -c -W /root/hello/test.pl'.split(' '), { env });
+		let result: Diagnostic[] = [];
 		return new Promise<Diagnostic[]>((resolve, reject) => {
-			let child = exec('docker exec perl perl -c -W /root/hello/test.pl',
-				{ env },
-				(error, stdout, stderr) => {
-					let result: Diagnostic[] = []
-					let message: string;
-					if (error) {
-						error.message.toString().split('\n').forEach(e => {
-							let hello = e.split(' at /root/hello/test.pl line ');
-							if (hello.length > 1) {
-								result.push({
-									start: { line: parseInt(hello[1]), character: 0 },
-									end: { line: parseInt(hello[1]), character: Number.MAX_VALUE },
-									severity: Severity.Error,
-									message: hello[0]
-								});
-							}
+			child.stderr.on('data', (data: Buffer) => {
+				let errStr = data.toString()
+				errStr.split('\n').forEach(line => {
+					let hello = line.split(' line ');
+					if (hello.length > 1) {
+						result.push({
+							start: { line: parseInt(hello[1]), character: 0 },
+							end: { line: parseInt(hello[1]), character: Number.MAX_VALUE },
+							severity: Severity.Error,
+							message: hello[0]
 						});
 					}
-					resolve(result)
-				})
+				});
+				resolve(result);
+			})
+			child.on('close', code => {
+			})
 		});
 	}
 };
