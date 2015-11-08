f4da0d599dde34d45eb5eb70351bbf85fb36bfdb
Henrik Sjööh <henriiik@gmail.com>
2015-11-08 09:36:24 +0100

    tslint extensions.ts

--- docker-linter/extension.ts
+++ docker-linter/extension.ts
@@ -1,6 +1,6 @@
 "use strict";
-import * as path from 'path';
-import { workspace, Disposable, ExtensionContext } from 'vscode';
+import * as path from "path";
+import { workspace, Disposable, ExtensionContext } from "vscode";
 import { LanguageClient, LanguageClientOptions, SettingMonitor, RequestType } from 'vscode-languageclient';
 
 let perlDefaults = {
@@ -15,15 +15,14 @@ let perlDefaults = {
 	code: 0
 };
 
-let langs = ['perl', 'perlcritic'];
+let langs = ["perl", "perlcritic"];
 
 export function activate(context: ExtensionContext) {
-
 	langs.forEach(lang => {
 
 		// We need to go one level up since an extension compile the js code into
 		// the output folder.
-		let serverModule = path.join(__dirname, '..', 'server', 'server.js');
+		let serverModule = path.join(__dirname, "..", "server", "server.js");
 		let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
 		let serverOptions = {
 			run: { module: serverModule, hello: perlDefaults },
@@ -31,13 +30,13 @@ export function activate(context: ExtensionContext) {
 		};
 
 		let clientOptions: LanguageClientOptions = {
-			documentSelector: ['perl'],
+			documentSelector: ["perl"],
 			synchronize: {
-				configurationSection: 'docker-linter-'+lang
+				configurationSection: "docker-linter-"+lang
 			}
-		}
+		};
 
-		let client = new LanguageClient('Docker Linter '+lang+'!', serverOptions, clientOptions);
-		context.subscriptions.push(new SettingMonitor(client, 'docker-linter-'+lang+'.enable').start());
-	})
+		let client = new LanguageClient("Docker Linter "+lang+"!", serverOptions, clientOptions);
+		context.subscriptions.push(new SettingMonitor(client, "docker-linter-"+lang+".enable").start());
+	});
 }
