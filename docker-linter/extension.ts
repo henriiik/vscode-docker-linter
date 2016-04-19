5751b1f85f35033cd553badc4309d88ce6e613a0
Henrik Sjööh <henriiik@gmail.com>
2016-04-19 19:08:13 +0200

    fix tslint

--- docker-linter/extension.ts
+++ docker-linter/extension.ts
@@ -5,46 +5,46 @@ import { workspace, Disposable, ExtensionContext } from "vscode";
 import { LanguageClient, LanguageClientOptions, SettingMonitor, RequestType } from "vscode-languageclient";
 
 interface DockerLinter {
-	name: string;
-	language: string;
+    name: string;
+    language: string;
 }
 
 let linters: DockerLinter[] = [
-	{
-		name: "perl",
-		language: "perl"
-	}, {
-		name: "perlcritic",
-		language: "perl"
-	}, {
-		name: "flake8",
-		language: "python"
-	}, {
-		name: "rubocop",
-		language: "ruby"
-	}
+    {
+        name: "perl",
+        language: "perl"
+    }, {
+        name: "perlcritic",
+        language: "perl"
+    }, {
+        name: "flake8",
+        language: "python"
+    }, {
+        name: "rubocop",
+        language: "ruby"
+    }
 ];
 
 export function activate(context: ExtensionContext) {
-	linters.forEach(linter => {
+    linters.forEach(linter => {
 
-		// We need to go one level up since an extension compile the js code into
-		// the output folder.
-		let serverModule = path.join(__dirname, "..", "server", "server.js");
-		let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
-		let serverOptions = {
-			run: { module: serverModule },
-			debug: { module: serverModule, options: debugOptions }
-		};
+        // We need to go one level up since an extension compile the js code into
+        // the output folder.
+        let serverModule = path.join(__dirname, "..", "server", "server.js");
+        let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+        let serverOptions = {
+            run: { module: serverModule },
+            debug: { module: serverModule, options: debugOptions }
+        };
 
-		let clientOptions: LanguageClientOptions = {
-			documentSelector: [linter.language],
-			synchronize: {
-				configurationSection: `docker-linter.${linter.name}`
-			}
-		};
+        let clientOptions: LanguageClientOptions = {
+            documentSelector: [linter.language],
+            synchronize: {
+                configurationSection: `docker-linter.${linter.name}`
+            }
+        };
 
-		let client = new LanguageClient(`Docker Linter: ${linter.name}`, serverOptions, clientOptions);
-		context.subscriptions.push(new SettingMonitor(client, `docker-linter.${linter.name}.enable`).start());
-	});
+        let client = new LanguageClient(`Docker Linter: ${linter.name}`, serverOptions, clientOptions);
+        context.subscriptions.push(new SettingMonitor(client, `docker-linter.${linter.name}.enable`).start());
+    });
 }
