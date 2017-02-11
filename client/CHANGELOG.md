ab24dbaf0bbed5a655b3189642841fe4506df707
Henrik Sjööh <henke@happy.local>
2017-02-11 15:00:50 +0100

    update folder structure

--- client/CHANGELOG.md
+++ client/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.0 (2017-02-11)
+
+- add support for php
+
 ## 0.2.0 (2016-01-06)
 
 - add support for fixed severity levels
--- playground-php/index.php
+++ playground-php/index.php
@@ -3,6 +3,6 @@
     <title>PHP Test</title>
 </head>
 <body>
-    <?php ech '<p>Hello World</p>' ?>
+    <?php echo '<p>Hello World</p>' ?>
 </body>
 </html>
